5aeb23b66578de99a71c64d4cdbc20b860f5bc25
MvdS <git@vandersluys.nl>
2022-04-29 14:31:40 +0200

    install-conda.sh: add conda config --set auto_activate_base false.
    * install-conda.sh: add conda config --set auto_activate_base false:
      + Don't show "(base) " prefix to prompt.

--- install-conda.sh
+++ install-conda.sh
@@ -18,6 +18,7 @@ echo "Installing Conda in $CondaDir/"
 time bash Miniforge3-Linux-x86_64.sh -b -p $CondaDir || err "installing Conda"
 source $CondaDir/bin/activate || err "activating Conda"
 conda init || err "initialising Conda"
+conda config --set auto_activate_base false  # Don't show "(base) " prefix to prompt.  Setting stored in ~/.condarc.
 
 # rm -f Miniforge3-Linux-x86_64.sh  ||  err "cleaning up"
 
